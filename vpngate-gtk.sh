479c902c4c0bf6e71750289e3c112fa0e72b8985
apicalshark <58538165+apicalshark@users.noreply.github.com>
2026-06-29 11:52:28 +0000

    update script

--- vpngate-gtk.sh
+++ vpngate-gtk.sh
@@ -2,10 +2,4 @@
 PYTHON=/usr/bin/python3
 APP_DIR=/usr/share/vpngate-gtk
 
-for pyc in "$APP_DIR"/__pycache__/main.*.pyc "$APP_DIR"/main.*.pyc; do
-  if [ -f "$pyc" ]; then
-    PYTHONPATH="$APP_DIR" exec "$PYTHON" "$pyc" "$@"
-  fi
-done
-
 PYTHONPATH="$APP_DIR" exec "$PYTHON" "$APP_DIR/main.py" "$@"
