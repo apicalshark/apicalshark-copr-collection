5f1772b0e20c3ad47e6f83a200411ee6199696fe
apicalshark <58538165+apicalshark@users.noreply.github.com>
2026-06-29 11:39:12 +0000

    use bytecode

--- vpngate-gtk.sh
+++ vpngate-gtk.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-exec /usr/bin/python3 /usr/share/vpngate-gtk/main.py "$@"
+PYTHON=/usr/bin/python3
+APP_DIR=/usr/share/vpngate-gtk
+
+for pyc in "$APP_DIR"/__pycache__/main.*.pyc "$APP_DIR"/main.*.pyc; do
+  if [ -f "$pyc" ]; then
+    exec "$PYTHON" "$pyc" "$@"
+  fi
+done
+
+# exec "$PYTHON" "$APP_DIR/main.py" "$@"
